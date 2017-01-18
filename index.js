49ce4023ffcdc81cc1e2eb2d4a5e93f53c34c905
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-18 14:48:32 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0
+    function addBump() {
+      counter++
+    }
+    function getBumps() {
+      return counter
+    }
+    return { addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  var animalType = 'Shark'
+  return function(deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+}
+
+
+var sharkCreator = createAnimal()
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
